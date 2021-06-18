6a46dec67fdade1857906cc5ad091bf29429ce66
Turo-Rudi <jozsa.andrea@gmail.com>
2021-06-18 23:17:40 +0200

    add morgan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
 
 // Logging with Morgan
 app.use(morgan('common'));
