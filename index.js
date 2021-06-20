ed9b619ea18f3ca68396b71f8d8d1e4bf2f17255
Turo-Rudi <jozsa.andrea@gmail.com>
2021-06-20 23:07:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const Models = require('./models.js');
 const passport = require('passport');
 require('./passport');
 const cors = require('cors');
-app.use(cors());
 const { check, validationResult } = require('express-validator');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const Movies = Models.Movie;
