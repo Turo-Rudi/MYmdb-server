41b9f1538edcd7300760dc5567549184a3df3320
Turo-Rudi <jozsa.andrea@gmail.com>
2021-06-20 21:57:11 +0200

    add user validation checks, update app.listen

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const passport = require('passport');
 require('./passport');
+const cors = require('cors');
+app.use(cors());
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +21,7 @@ const Genres = Models.Genre;
 
 let auth = require('./auth')(app);
 
+// DB connection
 mongoose.connect('mongodb://localhost:27017/MYmdb', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Logging with Morgan
@@ -32,7 +36,7 @@ app.get('/', (req, res) => {
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
-      res.status(201).json(movies);
+      res.status(200).json(movies);
     })
     .catch((error) => {
       console.error(error);
@@ -79,7 +83,7 @@ app.get('/director/:Name', passport.authenticate('jwt', { session: false }), (re
 //Get all users
 app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find().then((users) => {
-    res.status(201).json(users);
+    res.status(200).json(users);
   })
     .catch((error) => {
       console.error(error);
@@ -87,29 +91,43 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
     });
 });
 
-// Add user
-app.post('/users', (req, res) => {
-  Users.findOne({ Username: req.body.Username }).then((user) => {
-    if (user) {
-      return res.status(400).send(req.body.Username + ' already exists.');
-    } else {
-      Users.create({
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
-      })
-        .then((user) => { res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
+// Add user with validaton checks
+app.post('/users',
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username must contain only alphanumeric characters').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Birthday', 'Birthday must have a date format').isDate()
+  ], (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  }).catch((error) => {
-    console.error(error);
-    res.status(500).send('Error: ' + error);
+    console.log(req.body)
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username }).then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists.');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+      }
+    }).catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
   });
-});
 
 //Get user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -124,25 +142,36 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 //Updates user info
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, {
-    $set:
-    {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Birthday', 'Birthday must have a date format').isDate()
+  ], (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  },
-    { new: true }, (error, updatedUser) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-      } else {
-        res.json(updatedUser);
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set:
+      {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
       }
-    });
-});
+    },
+      { new: true }, (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        } else {
+          res.json(updatedUser);
+        }
+      });
+  });
 
 //Adds favorite movie to a user
 app.post('/users/:Username/Movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -193,7 +222,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 //Get all actors
 app.get('/actors', passport.authenticate('jwt', { session: false }), (req, res) => {
   Actors.find().then((actors) => {
-    res.status(201).json(actors);
+    res.status(200).json(actors);
   })
     .catch((error) => {
       console.error(error);
@@ -222,6 +251,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something is not working!');
 });
 
-app.listen(8080, () => {
-  console.log('My app is listening to Port 8080')
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
